b48f28007e8a97e9da5a16c9b2ef1d77df68e099
BettingIsCool <contact@bettingiscool.com>
2024-10-22 15:34:05 +0200

    Refactor bet data display in Streamlit for clarity
    Renamed DataFrame columns for better readability and formatted numerical values. Enhanced display with styled DataFrame and added a hyperlink column.

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ st_autorefresh(interval=5 * 1000, debounce=True, key="dataframerefresh")
 bets = db.get_log()
 bets_df = pd.DataFrame(data=bets)
 bets_df = bets_df.rename(columns={'starts': 'STARTS', 'sport_name': 'SPORT', 'league_name': 'LEAGUE', 'runner_home': 'RUNNER_HOME', 'runner_away': 'RUNNER_AWAY', 'selection': 'SELECTION', 'market': 'MARKET', 'line': 'LINE', 'prev_odds': 'PODDS', 'curr_odds': 'CODDS', 'droppct': 'DROP', 'oddstobeat': 'OTB', 'book_odds': 'BODDS', 'book_val': 'BVAL', 'book_name': 'BNAME', 'book_url': 'BURL', 'timestamp': 'TIMESTAMP'})
-bets_df = bets_df['STARTS', 'SPORT', 'LEAGUE', 'RUNNER_HOME', 'RUNNER_AWAY', 'SELECTION', 'MARKET', 'LINE', 'PODDS', 'CODDS', 'DROP', 'OTB', 'BODDS', 'BVAL', 'BNAME', 'BURL', 'TIMESTAMP']
+bets_df = bets_df['STARTS', 'SPORT', 'LEAGUE', 'RUNNER_HOME', 'RUNNER_AWAY', 'SELECTION', 'MARKET', 'LINE', 'PODDS', 'CODDS', 'DROP', 'OTB']
 styled_df = bets_df.format({'LINE': '{:g}'.format, 'PODDS': '{:,.3f}'.format, 'CODDS': '{:,.3f}'.format, 'BODDS': '{:,.3f}'.format, 'OTB': '{:,.3f}'.format, 'BVAL': '{:,.2%}'.format})
 st.dataframe(styled_df, column_config={"BURL": st.column_config.LinkColumn("BURL")})
 
