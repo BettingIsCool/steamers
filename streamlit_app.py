cfa2bf73d0ecc303abbea5a768dd3de11f6b3197
BettingIsCool <contact@bettingiscool.com>
2024-10-22 12:05:35 +0200

    Add auto-refresh functionality to streamlit app
    Integrated the `streamlit_autorefresh` package to automatically refresh the app every 5 seconds. Removed the manual while-loop and datetime-based refresh mechanism for cleaner and more efficient code. Updated `requirements.txt` to include the new package.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,12 @@ import streamlit as st
 # set_page_config() can only be called once per app page, and must be called as the first Streamlit command in your script.
 st.set_page_config(page_title="ChasingSteamers (Ultra Feed) by BettingIsCool", page_icon="♨️", layout="wide", initial_sidebar_state="expanded")
 
+import pandas as pd
+import db_steamers_remote as db
 from streamlit_autorefresh import st_autorefresh
 
 # update every 5 seconds
 st_autorefresh(interval=5 * 1000, debounce=True, key="dataframerefresh")
-
-import pandas as pd
-import db_steamers_remote as db
 
 bets = db.get_log()
 bets_df = pd.DataFrame(data=bets)
