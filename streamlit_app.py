60fe1be7a6478471f396976ab61bad14a1b267bd
BettingIsCool <contact@bettingiscool.com>
2024-10-22 15:41:45 +0200

    Add ID column to output and format bet data in Streamlit
    Included the 'id' column in the SQL query in db_steamers_remote.py. Adjusted Streamlit app to rename the new column, format several columns, and display the data with proper column configurations.

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ st_autorefresh(interval=10 * 1000, debounce=True, key="dataframerefresh")
 bets = db.get_log()
 bets_df = pd.DataFrame(data=bets)
 bets_df = bets_df.rename(columns={'start_time': 'STARTS', 'sport_name': 'SPORT', 'league_name': 'LEAGUE', 'runner_home': 'RUNNER_HOME', 'runner_away': 'RUNNER_AWAY', 'selection': 'SELECTION', 'market': 'MARKET', 'line': 'LINE', 'prev_odds': 'PODDS', 'curr_odds': 'CODDS', 'droppct': 'DROP', 'oddstobeat': 'OTB', 'book_odds': 'BODDS', 'book_val': 'BVAL', 'book_name': 'BNAME', 'book_url': 'BURL', 'timestamp': 'TIMESTAMP', 'id': 'ID'})
-styled_df = bets_df.format({'LINE': '{:g}'.format, 'PODDS': '{:,.3f}'.format, 'CODDS': '{:,.3f}'.format, 'BODDS': '{:,.3f}'.format, 'OTB': '{:,.3f}'.format, 'BVAL': '{:,.2%}'.format})
+styled_df = bets_df.style.format({'LINE': '{:g}'.format, 'PODDS': '{:,.3f}'.format, 'CODDS': '{:,.3f}'.format, 'BODDS': '{:,.3f}'.format, 'OTB': '{:,.3f}'.format, 'BVAL': '{:,.2%}'.format})
 st.dataframe(styled_df, column_config={"BURL": st.column_config.LinkColumn("BURL")})
 st.cache_data.clear()
 
