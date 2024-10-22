1147e4407baf463283e7154457a554ec340f435f
BettingIsCool <contact@bettingiscool.com>
2024-10-22 16:26:13 +0200

    Enable autoplay for audio in streamlit app
    Added the `autoplay=True` parameter to the `st.audio` function to ensure the audio file plays automatically when the component is loaded. This change helps improve the user experience by eliminating the need for manual activation.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 from filecmp import clear_cache
 
+import beepy
 import streamlit as st
 
 from config import BOOKS
@@ -33,5 +34,6 @@ bets_df = bets_df.rename(columns={'starts': 'STARTS', 'sport_name': 'SPORT', 'le
 styled_df = bets_df.style.format({'LINE': '{:g}'.format, 'PODDS': '{:,.3f}'.format, 'CODDS': '{:,.3f}'.format, 'BODDS': '{:,.3f}'.format, 'OTB': '{:,.3f}'.format, 'BVAL': '{:,.2%}'.format})
 st.dataframe(styled_df, column_config={"BURL": st.column_config.LinkColumn("BURL")})
 st.cache_data.clear()
-st.audio(data='bell-ringing-05.wav', format="audio/wav", autoplay=True)
+beep(sound='coin')
+
 
