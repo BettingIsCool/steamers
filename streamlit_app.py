09010f1e5bab8b4990bf7930a7379aaaeabaa05e
BettingIsCool <contact@bettingiscool.com>
2024-10-23 14:35:51 +0200

    Add user_dbid to session state upon login
    Moved the initialization of `user_dbid` to immediately after fetching the username to ensure it is available for subsequent operations. This change improves session management by ensuring `user_dbid` is set early in the process.

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,6 +67,8 @@ if 'users_fetched' not in st.session_state:
     st.session_state.users_fetched = True
 
 
+st.write(st.session_state.user_dbid in USER_DOMAIN_CHANGES.keys())
+
 # Allow only ONE session per user
 # See https://discuss.streamlit.io/t/right-way-to-manage-same-user-opening-multiple-sessions/25608
 
@@ -90,7 +92,7 @@ if st.session_state.session_id == toolkit.get_active_session(st.session_state.us
 
             bets_df = pd.DataFrame(data=bets)
 
-            st.write(st.session_state.user_dbid in USER_DOMAIN_CHANGES)
+
 
             # Change bookie domains
             #if st.session_state.user_dbid in USER_DOMAIN_CHANGES.keys():
