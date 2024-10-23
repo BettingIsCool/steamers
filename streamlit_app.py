48101395439bce6f1297e5cc455fff1943d9f578
BettingIsCool <contact@bettingiscool.com>
2024-10-23 14:46:34 +0200

    Fix user_dbid assignment and enable debug logging
    Assign the correct user_dbid by selecting the first element from the database query result. Additionally, enable debug logging to write whether the user_dbid is in USER_DOMAIN_CHANGES.keys().

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,7 +90,8 @@ if st.session_state.session_id == toolkit.get_active_session(st.session_state.us
 
             bets_df = pd.DataFrame(data=bets)
 
-            st.write(USER_DOMAIN_CHANGES[st.session_state.user_dbid])
+            for domain_original, domain_changed in USER_DOMAIN_CHANGES[st.session_state.user_dbid]:
+                st.write(domain_original, domain_changed)
 
             # Change bookie domains
             if st.session_state.user_dbid in USER_DOMAIN_CHANGES.keys():
