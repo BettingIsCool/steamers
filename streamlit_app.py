b9be9666f63d4d8b4113058660bf163754055819
BettingIsCool <contact@bettingiscool.com>
2024-10-23 15:10:25 +0200

    Update domain replacement logic for bets_df URLs
    Removed unnecessary hardcoded domain change and re-implemented dynamic domain replacement using USER_DOMAIN_CHANGES. Cleaned up

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,17 +89,11 @@ if st.session_state.session_id == toolkit.get_active_session(st.session_state.us
         if bets:
 
             bets_df = pd.DataFrame(data=bets)
-            bets_df['book_url'] = bets_df['book_url'].str.replace('.de', '.com')
 
             # Change bookie domains
-            #if st.session_state.user_dbid in USER_DOMAIN_CHANGES.keys():
-            #    for domain_original, domain_changed in USER_DOMAIN_CHANGES[st.session_state.user_dbid].items():
-            #        bets_df['book_url'].replace(domain_original, domain_changed, inplace=True)
-            #        st.write(domain_original, domain_changed)
-                    #for index, row in bets_df.iterrows():
-                    #    if domain_original in row['book_url']:
-                    #        st.write('yes')
-
+            if st.session_state.user_dbid in USER_DOMAIN_CHANGES.keys():
+                for domain_original, domain_changed in USER_DOMAIN_CHANGES[st.session_state.user_dbid].items():
+                    bets_df['book_url'].str.replace(domain_original, domain_changed)
 
             bets_df = bets_df.rename(columns={'starts': 'STARTS', 'sport_name': 'SPORT', 'league_name': 'LEAGUE', 'runner_home': 'RUNNER_HOME', 'runner_away': 'RUNNER_AWAY', 'selection': 'SELECTION', 'market': 'MARKET', 'line': 'LINE', 'prev_odds': 'PODDS', 'curr_odds': 'CODDS', 'droppct': 'DROP', 'oddstobeat': 'OTB', 'book_odds': 'BODDS', 'book_val': 'BVAL', 'book_name': 'BNAME', 'book_url': 'BURL', 'timestamp': 'TIMESTAMP', 'id': 'ID'})
             bets_df = bets_df[['TIMESTAMP', 'STARTS', 'SPORT', 'LEAGUE', 'RUNNER_HOME', 'RUNNER_AWAY', 'MARKET', 'SELECTION', 'LINE', 'BODDS', 'BVAL', 'BNAME', 'BURL', 'ID']]
