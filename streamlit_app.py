9b5d2188d3b2306aaefc65695bcb8c36758e9f68
BettingIsCool <contact@bettingiscool.com>
2024-10-23 21:14:40 +0200

    Add input fields for minimum and maximum odds
    Enhanced user settings by adding input fields for default_minodds and default_maxodds in the sidebar. These new fields allow users to specify and adjust the minimum and maximum odds thresholds according to their preferences.

--- streamlit_app.py
+++ streamlit_app.py
@@ -146,10 +146,10 @@ if st.session_state.session_id == toolkit.get_active_session(st.session_state.us
     st.session_state.default_minval = st.sidebar.number_input("Select minimum value threshold", min_value=0.025, max_value=1.00, value=st.session_state.default_minval, step=0.005, format="%0.3f", on_change=db.change_user_minval, args=(username, placeholder1), key='default_minval_key', help='Enter percentage as decimal number. 5% = 0.05')
 
     # Create number input for default_minodds
-    st.session_state.default_minodds = st.sidebar.number_input("Select minimum odds", min_value=1.00, max_value=1000, value=st.session_state.default_minodds, step=0.01, format="%0.2f", on_change=db.change_user_minodds, args=(username, placeholder1), key='default_minodds_key')
+    st.session_state.default_minodds = st.sidebar.number_input("Select minimum odds", min_value=1.00, max_value=1000.00, value=st.session_state.default_minodds, step=0.01, format="%0.2f", on_change=db.change_user_minodds, args=(username, placeholder1), key='default_minodds_key')
 
     # Create number input for default_maxodds
-    st.session_state.default_maxodds = st.sidebar.number_input("Select maximum odds", min_value=st.session_state.default_minodds, max_value=1000, value=st.session_state.default_maxodds, step=0.01, format="%0.2f", on_change=db.change_user_maxodds, args=(username, placeholder1), key='default_maxodds_key')
+    st.session_state.default_maxodds = st.sidebar.number_input("Select maximum odds", min_value=st.session_state.default_minodds, max_value=1000.00, value=st.session_state.default_maxodds, step=0.01, format="%0.2f", on_change=db.change_user_maxodds, args=(username, placeholder1), key='default_maxodds_key')
 
 
 
