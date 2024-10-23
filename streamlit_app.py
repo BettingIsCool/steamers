463788ddf37e2d58bf485fc582a811c2b7358753
BettingIsCool <contact@bettingiscool.com>
2024-10-23 23:29:30 +0200

    Improve success messages and sidebar input labels
    Updated success messages to be more specific in db_steamers_remote.py. Enhanced sidebar labels for clarity in streamlit_app.py, including adding a new input for selecting the default bookmaker.

--- streamlit_app.py
+++ streamlit_app.py
@@ -135,9 +135,9 @@ if st.session_state.session_id == toolkit.get_active_session(st.session_state.us
             # There is a possibility that the conversion fails if the timestamp falls into a time change
             # See https://github.com/streamlit/streamlit/issues/1288
             try:
-                bets_df.starts_utc = bets_df.starts_utc.dt.tz_localize('Europe/London').dt.tz_convert(st.session_state.timezone).dt.tz_localize(None)
+                bets_df.starts_utc = bets_df.starts_utc.dt.tz_localize('Europe/London').dt.tz_convert(st.session_state.default_timezone).dt.tz_localize(None)
             except Exception as ex:
-                st.write(ex)
+                pass
 
             # Change bookie domains
             if st.session_state.user_dbid in USER_DOMAIN_CHANGES.keys():
