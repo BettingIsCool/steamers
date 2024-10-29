968b3ce50c51055e041e2d7d59c9902ea9f1d30d
BettingIsCool <contact@bettingiscool.com>
2024-10-29 06:49:53 +0100

    Fix Telegram connection logic in sidebar
    Corrected the conditional check for the Telegram user ID to properly enable the 'Connect Telegram' button when no ID is present version control system mapping.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 TABLE_LOG, TABLE_USERS = 'log', 'users'
 
 USER_DOMAIN_CHANGES = dict()
-USER_DOMAIN_CHANGES.update({8: {'interwetten.de': 'interwetten.com', 'tipico.de': 'tipico.com'}})
+USER_DOMAIN_CHANGES.update({8: {'interwetten.de': 'interwetten.com', 'tipico.de': 'tipico.com', 'ladbrokes.com': 'ladbrokes.com.au'}})
 USER_DOMAIN_CHANGES.update({26: {'ladbrokes.com': 'ladbrokes.com.au'}})
 
 BOOKS = dict()
